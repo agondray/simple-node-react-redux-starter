7fbc1593b68f6dc35d3ed2d353e538410c763ef2
JV Estolas <jv@coverhound.com>
2017-10-05 01:09:19 -0700

    adds key to footer map function

--- src/assets/components/Footer/index.jsx
+++ src/assets/components/Footer/index.jsx
@@ -11,7 +11,7 @@ export default class App extends Component {
     const pseudoProps = ['Link to site A', 'Link to site B', 'Link to site B'];
 
     this.partial = pseudoProps.map((prop, index) => (
-      <div>
+      <div key={prop}>
         <h2 className="footer-heading">Sample Footer Heading {index + 1}</h2>
         <ul className="footer-list">
           <li><a className="footer-links" href="#">{prop}</a></li>
