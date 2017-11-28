0ee532daa35760f4cf36d36260b7230e8f607974
JV Estolas <jv@coverhound.com>
2017-11-27 21:07:30 -0800

    updates react PropTypes to use prop-types module

--- src/assets/containers/TopNav/index.jsx
+++ src/assets/containers/TopNav/index.jsx
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import AppBar from 'material-ui/AppBar';
 import IconButton from 'material-ui/IconButton';
 import NavigationClose from 'material-ui/svg-icons/navigation/close';
